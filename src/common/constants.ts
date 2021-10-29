7d6c4699995d52afb38307a4602cbc60b78df758
charlesmindee <charles@mindee.co>
2021-10-29 16:01:11 +0200

    feat: add heavier model + postprocessing

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,3 @@
 export const MODEL_NAME = "crnn";
 export const HEATMAP_CANVAS_ID = "heatmap-canvas";
+export const VOCAB = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~°àâéèêëîïôùûçÀÂÉÈËÎÏÔÙÛÇ£€¥¢฿"
