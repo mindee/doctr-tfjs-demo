2c0bbd30e843369d71699991a0a7bc95a9e25b28
fg-mindee <fg@mindee.co>
2021-11-09 17:26:20 +0100

    docs: Added copyright & license headers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,8 @@
+// Copyright (C) 2021, Mindee.
+
+// This program is licensed under the Apache License version 2.
+// See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
